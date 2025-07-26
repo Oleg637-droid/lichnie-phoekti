defe10a57f85349e0d93223a0c19088612dc07d4
Oleg637-droid <semenovo637@mail.ru>
2025-07-26 16:07:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/pos')
+def pos():
+    return render_template("pos.html")
+
+@app.route('/reports')
+def reports():
+    return render_template("reports.html")
+
+@app.route('/add')
+def add():
+    return render_template("add_product.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
