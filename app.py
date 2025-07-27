8ab193aa77b9b31345bd5ee171de654921f1adfd
Oleg637-droid <semenovo637@mail.ru>
2025-07-27 18:18:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 from flask import Flask, render_template
 from datetime import datetime
+import psycopg2
+import os
 
 app = Flask(__name__)
+
+
+DATABASE_URL = "postgresql://lichnie_phoekti_db_user:mYbzh2LygbjUE5zTUgMLu603Cia1yDKp@dpg-d22ug2u3jp1c739alq9g-a/lichnie_phoekti_db"
+
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
+
+@app.route('/init_db')
+def init_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            price NUMERIC NOT NULL,
+            quantity INTEGER NOT NULL
+        );
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "База данных и таблица созданы!"
+
 
 @app.route('/')
 def index():
@@ -9,7 +36,14 @@ def index():
 
 @app.route('/pos')
 def pos():
-    return render_template("pos.html")
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM products')
+    products = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("pos.html", products=products)
+
 
 @app.route('/reports')
 def reports():
